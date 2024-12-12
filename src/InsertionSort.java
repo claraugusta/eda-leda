babb929209c8deba35398416eebeb86241665610
clara <mc.augusta1052@gmail.com>
2024-12-12 15:46:20 -0300

    add insertion e selection sort algoritmos

---
+++ src/InsertionSort.java
@@ -0,0 +1,19 @@
+public class InsertionSort {
+    public static int[] insertionSort(int[] in){
+
+        for (int i = in.length - 1; i > 0; i--) {
+            int j = i;
+            while (j > 0 && in[j] < in[j-1]){
+                swap(in,j);
+                j--;
+            }
+        }
+        return in;
+    }
+
+    public static void swap(int[] in, int i){
+        int aux = in[i];
+        in[i] = in[i-1];
+        in[i-1] = aux;
+    }
+}
---
+++ src/SelectionSort.java
@@ -0,0 +1,21 @@
+public class SelectionSort {
+
+    public static int [] selectionSort(int[] in){
+
+        for (int j = 0; j < in.length; j++) {
+            int indexMenor = j;
+            for (int i = j + 1; i < in.length; i++) {
+                if (in[i] < in[indexMenor])
+                    indexMenor = i;
+            }
+            swap(in, indexMenor, j);
+        }
+        return in;
+    }
+
+    public static void swap(int[] in, int i, int j){
+        int aux = in[i];
+        in[i] = in[j];
+        in[j] = aux;
+    }
+}
